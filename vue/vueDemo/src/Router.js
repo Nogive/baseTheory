bc04e6e0dd5460f63e8227c9408c05c8c4e3422c
chendan <603446367@qq.com>
2018-05-23 22:54:15 +0800

    组件传参 路由表组件群

--- vue/vueDemo/src/Router.js
+++ vue/vueDemo/src/Router.js
@@ -10,20 +10,8 @@ const second={
 const home={
 	template:'<div>Home 内容</div>'
 }
-const firstFirst={
-	template:'<div>firstFirst 内容</div>'
-}
-const firstSecond={
-	template:'<div>firstSecond 内容</div>'
-}
-
-const child={
-	template:`
-		<div>
-			<h1>子路由</h1>
-			<router-view class="aaa"></router-view>
-		</div>
-	`
+const hehe={
+	template:'<div>hehe 内容</div>'
 }
 
 const router=new vueRouter({
@@ -32,48 +20,38 @@ const router=new vueRouter({
 	routes:[
 		{
 			path:'/',
-			component:home
+			name:'home title',
+			components:{
+				default:home,
+				left:first,
+				right:second
+			}
 		},
 		{
 			path:'/first',
-			component:child,
-			children:[
-				{
-					path:'/',
-					component:first
-				},
-				{
-					path:'first',
-					component:firstFirst
-				},
-				{
-					path:'second',
-					component:firstSecond
-				}
-			]
+			name:'first title',
+			components:{
+				default:hehe,
+				left:first,
+				right:second
+			}
 		},
-		{
-			path:'/second',
-			component:second
-		}
 	]
 })
+
 new Vue({
 	router,
 	template:`
 		<div id="demo">
 			<h1>导航</h1>
+			<p>{{$route.name}}</p>
 			<ol>
 				<li><router-link to="/">Home</router-link></li>
 				<li><router-link to="/first">First</router-link></li>
-				<ul>
-					<li><router-link to="/first/first">child1</router-link></li>
-					<li><router-link to="/first/second">child2</router-link></li>
-				</ul>
-				<li><router-link to="/second">Second</router-link></li>
 			</ol>
-			<router-view class="aaa"></router-view>
+			<router-view></router-view>
+			<router-view name="left" style="float:left;width:50%;height:100px;background-color:red"></router-view>
+			<router-view name="right" style="float:right;width:50%;height:100px;background-color:green"></router-view>
 		</div>
 	`
 }).$mount('#app');
-
