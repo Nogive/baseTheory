514d1b14f25262fdcb265ae8c3b44dc36a423aab
chendan <603446367@qq.com>
2018-05-25 17:39:52 +0800

    jsp

---
+++ myapp/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <p>
+      <router-link to="/">Home</router-link>
+      <router-link to="/foo">Go to Foo</router-link>
+      <router-link to="/bar">GO to bar</router-link>
+      <router-link to="/hello/123">GO to hello</router-link>
+    </p>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ myapp/src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <img src="../assets/logo.png">
+    <p>欢迎 Home</p>
+  </div>
+</template>
+<script>
+export default {
+  name:'Home',
+}
+</script>
+<style>
+
+</style>
+
