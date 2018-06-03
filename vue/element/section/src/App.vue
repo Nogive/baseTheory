05d6455b759c47a1b8f03f56458aac6c42ea25ca
chendan <603446367@qq.com>
2018-06-03 15:42:58 +0800

    axios

--- vue/element/section/src/App.vue
+++ vue/element/section/src/App.vue
@@ -4,24 +4,39 @@
     <i class="el-icon-edit"></i>
     <i class="el-icon-share"></i>
     <i class="el-icon-delete"></i>
-    <el-button icon="el-icon-search" type="primary">按钮</el-button>
+    <el-button icon="el-icon-search" type="primary" @click="get">按钮</el-button>
     <el-radio v-model="radio" label="1">备选项</el-radio>
     <el-radio v-model="radio" label="2">备选项</el-radio>
     <date></date>
+    <p>{{message}}</p>
   </div>
 </template>
 
 <script>
 import Date from './components/Date.vue'
+import axios from 'axios'
 export default {
   name: 'App',
   data(){
     return {
-      radio:'1'
+      radio:'1',
+      message:'lala'
     }
   },
   components:{
     Date
+  },
+  mounted(){
+    this.get();
+  },
+  methods:{
+    get(){
+      axios.get('https://api.github.com/users/itstrive').then(function(res){
+        this.message=res.data;
+      }.bind(this)).catch(function(err){
+        console.log(err);
+      })
+    }
   }
 }
 </script>
